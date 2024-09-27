162c2637dbe9f0c12e6ab33b68ead18fd2e96743
Markus Hoffrogge <mhoffrogge@gmail.com>
2024-09-27 12:25:57 +0200

    Add subproject folder for testing unleash in a subfolder project

---
+++ subproject/subfolder/app/src/main/java/io/github/mavenplugins/unleash/sandbox/app/SandboxMain.java
@@ -0,0 +1,9 @@
+package io.github.mavenplugins.unleash.sandbox.app;
+
+public class SandboxMain {
+
+  public static void main(String[] args) {
+    System.out.println("Hello from " + SandboxMain.class.getName() + "!");
+  }
+
+}
